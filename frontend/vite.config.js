19581bba98bad83f78b92dee4238853f42517ce4
Siddharth04-cpu <reddysiddharth04@gmail.com>
2025-07-31 23:48:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this 'base' property. It tells Vite the correct path for your assets on GitHub Pages.
+  base: '/E-commerce-internship-project/',
 })
